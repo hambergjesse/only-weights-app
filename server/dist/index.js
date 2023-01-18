bc97d7584956d2b825b8be1542e8e3f1d9e382c8
hambergjesse <hambergjesse@gmail.com>
2023-01-18 12:41:23 +0200

    mondodb setup

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,10 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
 app.get("/", (req, res) => {
     res.send("Express + TypeScript Server");
 });
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,23 @@
 import express, { Express, Request, Response } from "express";
+const { MongoClient, ServerApiVersion } = require("mongodb");
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err: String) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
