45ba91b8cb23854e856fae8c84a95699d4803fa9
hambergjesse <hambergjesse@gmail.com>
2023-01-19 10:24:16 +0200

    scss and file structure setup + temp logo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,22 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React /*, { useState, useEffect }*/ from "react";
+// import axios from "axios";
 
-interface Data {
-  id: string;
-  name: string;
-}
+// interface Data {
+//   id: string;
+//   name: string;
+// }
 
 const App = () => {
-  const [data, setData] = useState<Data[]>([]);
+  // const [data, setData] = useState<Data[]>([]);
 
-  useEffect(() => {
-    axios
-      .get<Data[]>("http://localhost:8000/api")
-      .then((res) => setData(res.data))
-      .catch((err) => console.log(err));
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get<Data[]>("http://localhost:8000/api")
+  //     .then((res) => setData(res.data))
+  //     .catch((err) => console.log(err));
+  // }, []);
 
-  return (
-    <div className="app__wrapper">
-      {data.map((item) => (
-        <p key={item.id}>{item.name}</p>
-      ))}
-    </div>
-  );
+  return <div className="app__wrapper"></div>;
 };
 
 export default App;
